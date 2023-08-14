f449a04064c442815649c6b3e9aa631a8441b630
hasna <hasna@gmail.com>
2023-08-14 13:09:17 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path("",views.index, name="index"),
+    path('post/<int:id>/', views.post, name='post'),
+    path("create",views.create, name="create"),
+    path("register",views.register, name="register"),
+    path('edit/<int:id>/',views.edit, name="edit"),
+    path('delete/<int:id>/',views.delete, name="delete"), 
+    
+]
